5650156e752260eec9e18db77128b5ae6f7ca0bd
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2022-05-23 16:48:15 +0900

    learn state and seState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const App = () => {
-  const profiles = [
-    { name: 'Taro', age: 10 },
-    { name: 'Tim', age: 12 },
-    { name: 'Sun', age: 7 },
-  ];
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
-};
+const App = () => <Counter></Counter>;
 
-const User = (props) => {
-  return (
-    <div>
-      HEY!, I am {props.name}, and {props.age} years old!
-    </div>
-  );
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number,
-};
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.decrement}>-1</button>
+      </>
+    );
+  }
+}
 
 export default App;
