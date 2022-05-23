4c26226974b08c4d60f1064bebdc05229e967796
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2022-05-23 15:08:39 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-function App() {
-  const greeting = 'Hey';
-  const dom = <h1 className="foo">{greeting} Bob</h1>;
+import React from 'react';
+
+// function App() {
+//   const greeting = 'Hey';
+//   const dom = <h1 className="foo">{greeting} Bob</h1>;
+//   return (
+//     <>
+//       <label htmlFor="bar">bar</label>
+//       <input
+//         type="text"
+//         onChange={() => {
+//           console.log('I am Clicked');
+//         }}
+//       />
+//     </>
+//   );
+// }
+
+const App = () => {
   return (
-    <>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        onChange={() => {
-          console.log('I am Clicked');
-        }}
-      />
-    </>
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
   );
-}
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
